604fd0152c4de2710899c3a98a01ab1836dbffee
AP <aleksandrpolishchuk@gmail.com>
2023-03-21 23:54:11 +0700

    add dbContext to Models

---
+++ backend/studentAPI/studentAPI/Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace studentAPI.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { 
+            optionsBuilder.UseSqlServer("Server=(local)\\sqlexpress;Database=studentDB;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True");
+        }
+    }
+}
